721d4f0d3d9b8aa18abd5341a3374c3530010b92
thiraanuask <110973916+thiraanuask@users.noreply.github.com>
2025-08-27 06:50:37 +0700

    Delete DummyC#.cs

--- WebApplication_ShopItemGame/DummyC#.cs
+++
@@ -1,8 +0,0 @@
-
-namespace WebApplication_ShopItemGame {
-    public class DummyC# {
-        public static void Hello() {
-            System.Console.WriteLine("Hello GitHub Linguist!");
-        }
-    }
-}
